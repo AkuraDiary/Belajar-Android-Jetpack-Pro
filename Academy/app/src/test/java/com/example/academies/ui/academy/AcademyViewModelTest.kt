4c292bd7cb9652ca6ee0c03c078d525b7183d53e
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2021-12-15 21:04:35 +0700

    added unit test for viewmodels

---
+++ Academy/app/src/test/java/com/example/academies/ui/academy/AcademyViewModelTest.kt
@@ -0,0 +1,23 @@
+package com.example.academies.ui.academy
+
+import org.junit.Assert.*
+import org.junit.Before
+
+import org.junit.Test
+
+class AcademyViewModelTest {
+
+    private lateinit var viewModel:AcademyViewModel
+
+    @Before
+    fun setUp(){
+        viewModel = AcademyViewModel()
+    }
+
+    @Test
+    fun getCourses() {
+        val courseEntities = viewModel.getCourses()
+        assertNotNull(courseEntities)
+        assertEquals(5, courseEntities.size)
+    }
+}
---
+++ Academy/app/src/test/java/com/example/academies/ui/bookmark/BookmarkViewModelTest.kt
@@ -0,0 +1,23 @@
+package com.example.academies.ui.bookmark
+
+import org.junit.Assert.*
+
+import org.junit.Before
+import org.junit.Test
+
+class BookmarkViewModelTest {
+
+    private lateinit var viewModel: BookmarkViewModel
+
+    @Before
+    fun setUp() {
+        viewModel = BookmarkViewModel()
+    }
+
+    @Test
+    fun getBookmarks() {
+        val courseEntities = viewModel.getBookmarks()
+        assertNotNull(courseEntities)
+        assertEquals(5, courseEntities.size)
+    }
+}
