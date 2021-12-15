e10cc9ffa588592fba60938fbc8376903b254dad
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2021-12-15 11:51:15 +0700

    added View Model for Academy UI

--- Academy/app/src/main/java/com/example/academies/ui/academy/AcademyFragment.kt
+++ Academy/app/src/main/java/com/example/academies/ui/academy/AcademyFragment.kt
@@ -1,24 +1,15 @@
 package com.example.academies.ui.academy
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.academies.databinding.FragmentAcademyBinding
-import com.example.academies.utils.DataDummy
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [AcademyFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class AcademyFragment : Fragment() {
 
     private lateinit var fragmentAcademyBinding: FragmentAcademyBinding
@@ -34,8 +25,10 @@ class AcademyFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         if (activity != null){
-            val courses = DataDummy.generateDummyCourses()
+            val viewModel = ViewModelProvider(this, ViewModelProvider.NewInstanceFactory())[AcademyViewModel::class.java]
+            val courses = viewModel.getCourses()
             val academyAdapter = AcademyAdapter()
+
             academyAdapter.setCourses(courses)
 
             with(fragmentAcademyBinding.rvAcademy){
