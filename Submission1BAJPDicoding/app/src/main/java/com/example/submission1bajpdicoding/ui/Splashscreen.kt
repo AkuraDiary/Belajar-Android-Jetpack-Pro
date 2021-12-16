025e9a024424e79a913f07d431facde54379696f
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2021-12-16 17:01:19 +0700

    added Splashscreen activity

---
+++ Submission1BAJPDicoding/app/src/main/java/com/example/submission1bajpdicoding/ui/Splashscreen.kt
@@ -0,0 +1,36 @@
+package com.example.submission1bajpdicoding.ui
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.submission1bajpdicoding.databinding.ActivitySplashscreenBinding
+
+@SuppressLint("CustomSplashScreen")
+class Splashscreen : AppCompatActivity() {
+
+    private lateinit var bindingSplash : ActivitySplashscreenBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        bindingSplash = ActivitySplashscreenBinding.inflate(layoutInflater)
+        setContentView(bindingSplash.root)
+
+        val background = object :Thread(){
+            override fun run() {
+                try {
+                    /* simulating some workloads here */
+                    sleep(2500)
+                    /* simulating some workloads here */
+
+                    //continue into next activity
+                    val intent = Intent(this@Splashscreen, MainActivity::class.java)
+                    startActivity(intent)
+                }catch (e : Exception){
+                    e.printStackTrace()
+                }
+            }
+        }
+        background.start()
+    }
+}
