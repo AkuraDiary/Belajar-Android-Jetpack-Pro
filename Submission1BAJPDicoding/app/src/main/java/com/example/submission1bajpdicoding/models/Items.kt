2dfb7ba5f79f9100939d6e1e62f0b9aeb11f10e9
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2021-12-16 20:52:27 +0700

    added models / object

---
+++ Submission1BAJPDicoding/app/src/main/java/com/example/submission1bajpdicoding/models/Items.kt
@@ -0,0 +1,16 @@
+package com.example.submission1bajpdicoding.models
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class Items(
+    val id : Int? = 0,
+    var judul: String? = null,
+    var waktuRelease: String? = null,
+    var genre: String? = null,
+    var durasi: String? = null,
+    var score: String? = null,
+    var overview: String? = null,
+    var poster: Int? = 0
+): Parcelable
