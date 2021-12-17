5fb53e1269bb32e6d9c330f9832b7908e4824559
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2021-12-17 21:37:22 +0700

    added resources and code for TV Shows

--- Submission1BAJPDicoding/app/src/main/java/com/example/submission1bajpdicoding/utilities/DataDummy.kt
+++ Submission1BAJPDicoding/app/src/main/java/com/example/submission1bajpdicoding/utilities/DataDummy.kt
@@ -4,10 +4,8 @@ import com.example.submission1bajpdicoding.R
 import com.example.submission1bajpdicoding.models.Items
 
 object DataDummy {
-    val listOfMovies = ArrayList<Items>()
+    private val listOfMovies = ArrayList<Items>()
     val listOfTV = ArrayList<Items>()
-
-
 
     fun getMovie(): ArrayList<Items> {
 
@@ -187,13 +185,192 @@ object DataDummy {
     }
 
     fun movieDetails(id:Int){
-        TODO("Not yet Implemented")
+        getMovie()[id]
     }
 
-    fun getTVShow(){
-        TODO("Not yet Implemented")
+    fun getTVShow():ArrayList<Items>{
+        listOfTV.add(
+            Items(
+                0,
+                "Naruto Shippuden",
+                "02/15/2007",
+                "Animation, Comedy, Drama",
+                "25 minutes / episode",
+                "87%",
+                "Naruto Shippuuden is the continuation of the original animated TV series " +
+                        "Naruto.The story revolves around an older and slightly more matured Uzumaki " +
+                        "Naruto and his quest to save his friend Uchiha Sasuke from the grips of the " +
+                        "snake-like Shinobi, Orochimaru. After 2 and a half years Naruto finally " +
+                        "returns to his village of Konoha, and sets about putting his ambitions to " +
+                        "work, though it will not be easy, as He has amassed a few (more dangerous) " +
+                        "enemies, in the likes of the shinobi organization; Akatsuki.",
+                R.drawable.poster_naruto_shipudden
+            )
+        )
+
+        listOfTV.add(
+            Items(
+                1,
+                "NCIS",
+                "09/03/2003",
+                "Action & Adventure, Crime, Drama",
+                "45 minutes / episode",
+                "71%",
+                "From murder and espionage to terrorism and stolen submarines, a team of " +
+                        "special agents investigates any crime that has a shred of evidence connected " +
+                        "to Navy and Marine Corps personnel, regardless of rank or position.",
+                R.drawable.poster_ncis
+            )
+        )
+
+        listOfTV.add(
+            Items(
+                2,
+                "The Walking Dead",
+                "10/31/2010",
+                "Action & Adventure, Drama, Sci-Fi & Fantasy",
+                "42 minutes / episode",
+                "79%",
+                "heriffs deputy Rick Grimes awakens from a coma to find a post-apocalyptic " +
+                        "world dominated by flesh-eating zombies. He sets out to find his family and " +
+                        "encounters many other survivors along the way.",
+                R.drawable.poster_the_walking_dead
+
+            )
+        )
+
+        listOfTV.add(
+            Items(
+                3,
+                " Marvels Iron Fist",
+                "03/17/2017",
+                "Action & Adventure, Drama, Sci-Fi & Fantasy",
+                "55 minutes / episode",
+                "64%",
+                "Danny Rand resurfaces 15 years after being presumed dead. Now, with the " +
+                        "power of the Iron Fist, he seeks to reclaim his past and fulfill his destiny",
+                R.drawable.poster_iron_fist
+            )
+        )
+
+        listOfTV.add(
+            Items(
+                4,
+                "Family Guy",
+                "01/31/1999",
+                "Animation, Comedy",
+                "22 minutes / episode",
+                "68%",
+                "Sick, twisted, politically incorrect and Freakin Sweet animated series " +
+                        "featuring the adventures of the dysfunctional Griffin family. Bumbling Peter" +
+                        " and long-suffering Lois have three kids. Stewie (a brilliant but sadistic " +
+                        "baby bent on killing his mother and taking over the world), Meg (the oldest," +
+                        " and is the most unpopular girl in town) and Chris (the middle kid, he is not" +
+                        " very bright but has a passion for movies). The final member of the family " +
+                        "is Brian - a talking dog and much more than a pet, he keeps Stewie in check " +
+                        "whilst sipping Martinis and sorting through his own life issues.",
+                R.drawable.poster_family_guy
+            )
+        )
+
+        listOfTV.add(
+            Items(
+                5,
+                "Fairy Tail",
+                "10/12/2009",
+                "Action & Adventure, Animation, Comedy, Sci-Fi & Fantasy",
+                "25 minutes / episode",
+                "75%",
+                "Lucy is a 17-year-old girl, who wants to be a full-fledged mage. One day when" +
+                        " visiting Harujion Town, she meets Natsu, a young man who gets sick easily" +
+                        " by any type of transportation. But Natsu isnt just any ordinary kid, hes " +
+                        "a member of one of the worlds most infamous mage guilds: Fairy Tail.",
+                R.drawable.poster_fairytail
+            )
+        )
+
+        listOfTV.add(
+            Items(
+                6,
+                "Family Guy",
+                "01/31/1999",
+                "Animation, Comedy",
+                "22 minutes / episode",
+                "68%",
+                "Sick, twisted, politically incorrect and Freakin Sweet animated series " +
+                        "featuring the adventures of the dysfunctional Griffin family. Bumbling Peter" +
+                        " and long-suffering Lois have three kids. Stewie (a brilliant but sadistic " +
+                        "baby bent on killing his mother and taking over the world), Meg (the oldest," +
+                        " and is the most unpopular girl in town) and Chris (the middle kid, he is not" +
+                        " very bright but has a passion for movies). The final member of the family " +
+                        "is Brian - a talking dog and much more than a pet, he keeps Stewie in check " +
+                        "whilst sipping Martinis and sorting through his own life issues.",
+                R.drawable.poster_family_guy
+            )
+        )
+
+        listOfTV.add(
+            Items(
+                7,
+                "The Flash",
+                "10/07/2014",
+                "Drama, Sci-Fi & Fantasy",
+                "44 minutes / episode",
+                "75%",
+                "After a particle accelerator causes a freak storm, CSI Investigator Barry " +
+                        "Allen is struck by lightning and falls into a coma. Months later he awakens " +
+                        "with the power of super speed, granting him the ability to move through Central " +
+                        "City like an unseen guardian angel. Though initially excited by his newfound " +
+                        "powers, Barry is shocked to discover he is not the only meta-human who " +
+                        "was created in the wake of the accelerator explosion -- and not everyone is " +
+                        "using their new powers for good. Barry partners with S.T.A.R. Labs and " +
+                        "dedicates his life to protect the innocent. For now, only a few close friends " +
+                        "and associates know that Barry is literally the fastest man alive, but it " +
+                        "wont be long before the world learns what Barry Allen has become...The Flash.",
+                R.drawable.poster_flash
+            )
+        )
+
+        listOfTV.add(
+            Items(
+                8,
+                "Supergirl",
+                "10/25/2016",
+                "Action, Adventure, Drama, Science Fiction",
+                "42 minutes / episode",
+                "71%",
+                "Twenty-four-year-old Kara Zor-El, who was taken in by the Danvers family " +
+                        "when she was 13 after being sent away from Krypton, must learn to embrace " +
+                        "her powers after previously hiding them. The Danvers teach her to be careful" +
+                        " with her powers, until she has to reveal them during an unexpected disaster, " +
+                        "setting her on her journey of heroism.",
+                R.drawable.poster_supergirl
+            )
+        )
+
+        listOfTV.add(
+            Items(
+                9,
+                "Supernatural",
+                "10/13/2005",
+                "Drama, Mystery, Sci-Fi & Fantasy",
+                "45 minutes/ episode",
+                "81%",
+                "When they were boys, Sam and Dean Winchester lost their mother to a " +
+                        "mysterious and demonic supernatural force. Subsequently, their father raised " +
+                        "them to be soldiers. He taught them about the paranormal evil that lives in " +
+                        "the dark corners and on the back roads of America ... and he taught them " +
+                        "how to kill it. Now, the Winchester brothers crisscross the country in their " +
+                        "67 Chevy Impala, battling every kind of supernatural threat they encounter " +
+                        "along the way.",
+                R.drawable.poster_supernatural
+            )
+        )
+
+        return listOfTV
     }
     fun tvShowDetails(id:Int){
         TODO("Not yet Implemented")
     }
+
 }
