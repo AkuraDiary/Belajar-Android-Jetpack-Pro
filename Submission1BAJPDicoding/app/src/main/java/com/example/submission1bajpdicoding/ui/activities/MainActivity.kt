37b710a4c6746f2e1f68bd6b7d079daebd2cd2a4
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2021-12-18 01:41:31 +0700

    added some code for main activity

--- Submission1BAJPDicoding/app/src/main/java/com/example/submission1bajpdicoding/ui/activities/MainActivity.kt
+++ Submission1BAJPDicoding/app/src/main/java/com/example/submission1bajpdicoding/ui/activities/MainActivity.kt
@@ -3,10 +3,20 @@ package com.example.submission1bajpdicoding.ui.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.submission1bajpdicoding.R
+import com.example.submission1bajpdicoding.databinding.ActivityMainBinding
+import com.example.submission1bajpdicoding.utilities.PagerSectionAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_main)
+
+        val pagerSectionAdapter = PagerSectionAdapter(this, supportFragmentManager)
+        main_toolbar.title = getString(R.string.app_name)
+        main_viewPager.adapter = pagerSectionAdapter
+        main_tabs.setupWithViewPager(main_viewPager)
     }
 }
