f9c77a0ea6444bb4fe1f48f27ecf751c55591359
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2021-12-18 11:41:34 +0700

    refactoring on main (and changed the color)

--- Submission1BAJPDicoding/app/src/main/java/com/example/submission1bajpdicoding/ui/activities/MainActivity.kt
+++ Submission1BAJPDicoding/app/src/main/java/com/example/submission1bajpdicoding/ui/activities/MainActivity.kt
@@ -1,11 +1,13 @@
 package com.example.submission1bajpdicoding.ui.activities
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.submission1bajpdicoding.R
 import com.example.submission1bajpdicoding.databinding.ActivityMainBinding
 import com.example.submission1bajpdicoding.utilities.PagerSectionAdapter
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.list_item_layout.view.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,7 +18,11 @@ class MainActivity : AppCompatActivity() {
         supportActionBar?.hide()
         val pagerSectionAdapter = PagerSectionAdapter(this, supportFragmentManager)
         main_toolbar.title = getString(R.string.app_name)
+
+        main_toolbar.setTitleTextColor(Color.WHITE)
+        main_tabs.setTabTextColors(Color.WHITE, Color.CYAN)
         main_viewPager.adapter = pagerSectionAdapter
+
         main_tabs.setupWithViewPager(main_viewPager)
     }
 }
