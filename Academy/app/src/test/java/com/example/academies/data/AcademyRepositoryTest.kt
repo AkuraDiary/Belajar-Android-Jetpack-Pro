a34a0e71941dba0e81627d3654be99b63ba7c360
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2021-12-21 16:28:49 +0700

    added Academy Repository Test

---
+++ Academy/app/src/test/java/com/example/academies/data/AcademyRepositoryTest.kt
@@ -0,0 +1,67 @@
+package com.example.academies.data
+
+import com.example.academies.data.source.remote.RemoteDataSource
+import com.example.academies.utils.DataDummy
+import junit.framework.TestCase.assertEquals
+import junit.framework.TestCase.assertNotNull
+import org.junit.Test
+import org.mockito.Mockito
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.verify
+
+class AcademyRepositoryTest {
+    private val remote = Mockito.mock(RemoteDataSource::class.java)
+    private val academyRepository = FakeAcademyRepository(remote)
+
+    private val courseResponses = DataDummy.generateRemoteDummyCourses()
+    private val courseId = courseResponses[0].id
+    private val moduleResponses = DataDummy.generateRemoteDummyModules(courseId)
+    private val moduleId = moduleResponses[0].moduleId
+    private val content = DataDummy.generateRemoteDummyContent(moduleId)
+
+    @Test
+    fun getAllCourses() {
+        `when`(remote.getAllCourses()).thenReturn(courseResponses)
+        val courseEntities = academyRepository.getAllCourses()
+        verify(remote).getAllCourses()
+        assertNotNull(courseEntities)
+        assertEquals(courseResponses.size.toLong(), courseEntities.size.toLong())
+    }
+
+    @Test
+    fun getAllModulesByCourse() {
+        `when`(remote.getModules(courseId)).thenReturn(moduleResponses)
+        val moduleEntities = academyRepository.getAllModulesByCourse(courseId)
+        verify(remote).getModules(courseId)
+        assertNotNull(moduleEntities)
+        assertEquals(moduleResponses.size.toLong(), moduleEntities.size.toLong())
+    }
+
+    @Test
+    fun getBookmarkedCourses() {
+        `when`(remote.getAllCourses()).thenReturn(courseResponses)
+        val courseEntities = academyRepository.getBookmarkedCourses()
+        verify(remote).getAllCourses()
+        assertNotNull(courseEntities)
+        assertEquals(courseResponses.size.toLong(), courseEntities.size.toLong())
+    }
+
+    @Test
+    fun getContent() {
+        `when`(remote.getModules(courseId)).thenReturn(moduleResponses)
+        `when`(remote.getContent(moduleId)).thenReturn(content)
+        val resultModule = academyRepository.getContent(courseId, moduleId)
+        verify(remote).getContent(moduleId)
+        assertNotNull(resultModule)
+        assertEquals(content.content, resultModule.contentEntity?.content)
+    }
+
+    @Test
+    fun getCourseWithModules() {
+        `when`(remote.getAllCourses()).thenReturn(courseResponses)
+        val resultCourse = academyRepository.getCourseWithModules(courseId)
+        verify(remote).getAllCourses()
+        assertNotNull(resultCourse)
+        assertEquals(courseResponses[0].title, resultCourse.title)
+    }
+}
