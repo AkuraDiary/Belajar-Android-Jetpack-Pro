a755e090f35e4c86ad1ac1b0446a471e8d960872
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2021-12-23 12:18:05 +0700

    add main activity codes

--- MyIdlingResource/app/src/main/java/com/example/myidlingresource/MainActivity.kt
+++ MyIdlingResource/app/src/main/java/com/example/myidlingresource/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.myidlingresource
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.example.myidlingresource.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var activityMainBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
+
+        activityMainBinding.button.setOnClickListener{
+            delay1()
+        }
+    }
+
+    private fun delay1(){
+        Handler(Looper.getMainLooper()).postDelayed({
+            activityMainBinding.textView.text = getString(R.string.delay1)
+        }, 2000)
     }
 }
