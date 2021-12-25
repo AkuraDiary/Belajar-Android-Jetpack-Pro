89fd09a041d5914d13e25aaa4f8753d17b54f5fd
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2021-12-25 19:08:18 +0700

    refactoring and added some dependencies

--- Submission2BAJPDicoding/app/src/main/java/com/example/submission1bajpdicoding/utilities/DetailsDataBinding.kt
+++
@@ -1,9 +0,0 @@
-package com.example.submission1bajpdicoding.utilities
-
-import android.widget.ImageView
-import com.example.submission1bajpdicoding.models.Items
-
-interface DetailsDataBinding {
-    fun setBinding(items: Items)
-    fun multipleGlide(firstImage: ImageView, secondImage: ImageView, items: Items)
-}
