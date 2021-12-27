718e2510b6f80952c9c30afa2ce3fda06b66a55e
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2021-12-27 09:08:15 +0700

    added some unit testing (just the files)

--- Submission2BAJPDicoding/app/src/test/java/com/example/submission2bajpdicoding/ui/detail/DetailViewModelTest.kt
+++ Submission2BAJPDicoding/app/src/test/java/com/example/submission2bajpdicoding/ui/detail/DetailViewModelTest.kt
@@ -1,10 +1,10 @@
-package com.example.submission2bajpdicoding.ui.movies
+package com.example.submission2bajpdicoding.ui.detail
 
 import org.junit.Assert.*
 
 import org.junit.Before
 
-class MovieViewModelTest {
+class DetailViewModelTest {
 
     @Before
     fun setUp() {
--- Submission2BAJPDicoding/app/src/test/java/com/example/submission2bajpdicoding/ui/tvShows/TvViewModelTest.kt
+++ Submission2BAJPDicoding/app/src/test/java/com/example/submission2bajpdicoding/ui/tvShows/TvViewModelTest.kt
@@ -1,10 +1,10 @@
-package com.example.submission2bajpdicoding.ui.movies
+package com.example.submission2bajpdicoding.ui.tvShows
 
 import org.junit.Assert.*
 
 import org.junit.Before
 
-class MovieViewModelTest {
+class TvViewModelTest {
 
     @Before
     fun setUp() {
