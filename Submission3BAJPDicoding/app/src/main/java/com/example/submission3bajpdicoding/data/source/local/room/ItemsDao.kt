7e71190d0a16895073ea35df6edba7ea534c3301
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-01-03 10:49:41 +0700

    finishing locals, i suppose

---
+++ Submission3BAJPDicoding/app/src/main/java/com/example/submission3bajpdicoding/data/source/local/room/ItemsDao.kt
@@ -0,0 +1,39 @@
+package com.example.submission3bajpdicoding.data.source.local.room
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import androidx.sqlite.db.SupportSQLiteQuery
+import com.example.submission3bajpdicoding.data.source.local.entity.Items
+import androidx.paging.DataSource
+
+@Dao
+interface ItemsDao {
+
+    @RawQuery(observedEntities = [Items::class])
+    fun getAllMovies(query: SupportSQLiteQuery): DataSource.Factory<Int, Items>
+
+    @RawQuery(observedEntities = [Items::class])
+    fun getAllTvShows(query: SupportSQLiteQuery): DataSource.Factory<Int, Items>
+
+    @Transaction
+    @Query("SELECT * FROM tabel_items WHERE id = :movieId")
+    fun getMovieById(movieId: Int): LiveData<Items>
+
+    @Transaction
+    @Query("SELECT * FROM tabel_items WHERE id = :tvShowId")
+    fun getTvShowById(tvShowId: Int): LiveData<Items>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertDataMovie(movies: List<Items>)
+
+    @Update
+    fun updateDataMovie(movies: Items)
+
+    @Query("SELECT * FROM tabel_items WHERE isFavorite = 1 AND isTvShow = 0")
+    fun getFavoriteMovies(): DataSource.Factory<Int, Items>
+
+    @Query("SELECT * FROM tabel_items WHERE isFavorite = 1 AND isTvShow = 1")
+    fun getFavoriteTvShow(): DataSource.Factory<Int, Items>
+
+
+}
---
+++ Submission3BAJPDicoding/app/src/main/java/com/example/submission3bajpdicoding/data/source/local/room/ItemsDatabase.kt
@@ -0,0 +1,4 @@
+package com.example.submission3bajpdicoding.data.source.local.room
+
+class ItemsDatabase {
+}
