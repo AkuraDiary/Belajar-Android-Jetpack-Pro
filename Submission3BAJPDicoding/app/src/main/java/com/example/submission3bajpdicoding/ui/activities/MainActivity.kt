6f4de93e4223c7c021885c2feaa406c312f230d5
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-01-06 20:29:07 +0700

    added code in main activity

--- Submission3BAJPDicoding/app/src/main/java/com/example/submission3bajpdicoding/ui/activities/MainActivity.kt
+++ Submission3BAJPDicoding/app/src/main/java/com/example/submission3bajpdicoding/ui/activities/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.example.submission3bajpdicoding.ui.activities
 
+import android.content.Intent
 import android.graphics.Color
 import android.os.Bundle
+import android.provider.Settings
 import androidx.appcompat.app.AppCompatActivity
 import com.example.submission3bajpdicoding.R
 import com.example.submission3bajpdicoding.databinding.ActivityMainBinding
+import com.example.submission3bajpdicoding.ui.favorite.FavoriteActivity
 import com.example.submission3bajpdicoding.utilities.PagerSectionAdapter
 
 class MainActivity : AppCompatActivity() {
@@ -13,16 +16,29 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        val viewPagerAdapter = HomeViewPagerAdapter(this, supportFragmentManager)
+
+        activityMainBinding.mainToolbar.setTitleTextColor(Color.WHITE)
+        activityMainBinding.mainTabs.setTabTextColors(Color.WHITE, Color.CYAN)
 
         setContentView(activityMainBinding.root)
         supportActionBar?.hide()
-        val pagerSectionAdapter = PagerSectionAdapter(this, supportFragmentManager)
+
+        activityMainBinding.mainViewPager.adapter = viewPagerAdapter
+        activityMainBinding.mainTabs.setupWithViewPager(activityMainBinding.mainViewPager)
+
+        activityMainBinding.ivFavoriteMain.setOnClickListener {
+            val intent = Intent(this, FavoriteActivity::class.java)
+            startActivity(intent)
+        }
+
+        /*val pagerSectionAdapter = PagerSectionAdapter(this, supportFragmentManager)
         activityMainBinding.mainToolbar.title = getString(R.string.app_name)
 
         activityMainBinding.mainViewPager.adapter = pagerSectionAdapter
 
         activityMainBinding.mainToolbar.setTitleTextColor(Color.WHITE)
         activityMainBinding.mainTabs.setTabTextColors(Color.WHITE, Color.CYAN)
-        activityMainBinding.mainTabs.setupWithViewPager(activityMainBinding.mainViewPager)
+        activityMainBinding.mainTabs.setupWithViewPager(activityMainBinding.mainViewPager)*/
     }
 } 
