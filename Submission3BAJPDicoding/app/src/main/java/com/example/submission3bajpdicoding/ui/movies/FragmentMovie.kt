8531a0673f763190fa79b513b842e6a84349c105
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-01-07 09:11:44 +0700

    added fragment and view model in Movie

--- Submission3BAJPDicoding/app/src/main/java/com/example/submission3bajpdicoding/ui/movies/FragmentMovie.kt
+++ Submission3BAJPDicoding/app/src/main/java/com/example/submission3bajpdicoding/ui/movies/FragmentMovie.kt
@@ -5,14 +5,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.paging.PagedList
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.submission3bajpdicoding.data.source.local.entity.Items
 import com.example.submission3bajpdicoding.databinding.FragmentMovieBinding
 import com.example.submission3bajpdicoding.ui.adapter.KatalogAdapter
+import com.example.submission3bajpdicoding.ui.adapter.MovieAdapter
 import com.example.submission3bajpdicoding.utilities.ViewModelFactory
+import com.example.submission3bajpdicoding.vo.Resource
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class FragmentMovie : Fragment() {
     private lateinit var binding : FragmentMovieBinding
+
+    private lateinit var moviesAdapter : MovieAdapter
+    private val viewModel : MovieViewModel by viewModel()
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -24,7 +34,10 @@ class FragmentMovie : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        if (activity != null){
+
+        moviesAdapter = MovieAdapter()
+
+        /*if (activity != null){
             val factory = ViewModelFactory.getInstance()
             val viewModel = ViewModelProvider(this, factory)[MovieViewModel::class.java]
             val movieAdapter = KatalogAdapter(EXTRA_CLICK_M)
@@ -39,7 +52,16 @@ class FragmentMovie : Fragment() {
                 setHasFixedSize(true)
                 adapter = movieAdapter
             }
-        }
+        }*/
+
+    }
+
+    private fun setList(sort : String){
+        viewModel.getMovies(sort).observe(requireActivity(), moviesObserver)
+    }
+
+    private val moviesObserver = Observer<Resource<PagedList<Items>>>{
+        TODO("belum di buat")
     }
 
     companion object{
--- Submission3BAJPDicoding/app/src/main/java/com/example/submission3bajpdicoding/ui/movies/MovieViewModel.kt
+++ Submission3BAJPDicoding/app/src/main/java/com/example/submission3bajpdicoding/ui/movies/MovieViewModel.kt
@@ -2,11 +2,13 @@ package com.example.submission3bajpdicoding.ui.movies
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.paging.PagedList
 import com.example.submission3bajpdicoding.data.source.ItemsRepository
 import com.example.submission3bajpdicoding.data.source.local.entity.Items
+import com.example.submission3bajpdicoding.vo.Resource
 
 class MovieViewModel(private val itemsRepository: ItemsRepository) : ViewModel(){
-    fun getMovies(): LiveData<List<Items>>{
-        return itemsRepository.getAllMovies()
+    fun getMovies(sort:String): LiveData<Resource<PagedList<Items>>>{
+        return itemsRepository.getAllMovies(sort)
     }
 }
