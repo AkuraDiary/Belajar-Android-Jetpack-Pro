c14df3ba67c976f319a7f331e2cfbfe29043dc62
Muhammad Asthi Seta Ari Yuwana <68726268+AkuraDiary@users.noreply.github.com>
2022-01-14 05:21:40 +0700

    added utilities for detail view model test

--- Submission3BAJPDicoding/app/src/test/java/com/example/submission3bajpdicoding/data/source/FakeItemRepository.kt
+++ Submission3BAJPDicoding/app/src/test/java/com/example/submission3bajpdicoding/data/source/FakeItemRepository.kt
@@ -1,4 +1,205 @@
 package com.example.submission3bajpdicoding.data.source
 
-class FakeItemRepository {
+import androidx.lifecycle.LiveData
+import androidx.paging.LivePagedListBuilder
+import androidx.paging.PagedList
+import com.example.submission3bajpdicoding.data.source.local.LocalDataSource
+import com.example.submission3bajpdicoding.data.source.local.entity.Items
+import com.example.submission3bajpdicoding.data.source.remote.ApiResponse
+import com.example.submission3bajpdicoding.data.source.remote.RemoteDataSource
+import com.example.submission3bajpdicoding.data.source.remote.response.MovieItem
+import com.example.submission3bajpdicoding.data.source.remote.response.TvShowItems
+import com.example.submission3bajpdicoding.utilities.AppExecutors
+import com.example.submission3bajpdicoding.vo.Resource
+
+class FakeItemRepository(
+    private val remoteDataSource: RemoteDataSource,
+    private val localDataSource: LocalDataSource,
+    private val appExecutors: AppExecutors
+) : ItemsDataSource{
+
+    override fun getAllMovies(sort:String): LiveData<Resource<PagedList<Items>>> {
+        return object :
+            NetworkBoundResource<PagedList<Items>, List<MovieItem>>(appExecutors) {
+            override fun loadFromDB(): LiveData<PagedList<Items>> {
+                val config =PagedList.Config.Builder()
+                    .setEnablePlaceholders(false)
+                    .setInitialLoadSizeHint(4)
+                    .setPageSize(4)
+                    .build()
+                return LivePagedListBuilder(localDataSource.getAllMovies(sort), config).build()
+            }
+
+            override fun shouldFetch(data: PagedList<Items>?): Boolean {
+                return data == null || data.isEmpty()
+            }
+
+            override fun createCall(): LiveData<ApiResponse<List<MovieItem>>> {
+                return remoteDataSource.getMovies()
+            }
+
+            override fun saveCallResult(data: List<MovieItem>) {
+                val movieList = ArrayList<Items>()
+                for(response in data){
+                    val movie = Items(
+                        response.id,
+                        response.title,
+                        response.releaseDate,
+                        response.originalTitle,
+                        response.popularity,
+                        response.voteAverage,
+                        response.overview,
+                        response.posterPath,
+                        isFavorite = false,
+                        isTvShow = false
+                    )
+                    movieList.add(movie)
+                }
+                localDataSource.insertItems(movieList)
+            }
+        }.asLiveData()
+    }
+
+
+    override fun getMovieById(Id: Int): LiveData<Resource<Items>> {
+        return object :
+            NetworkBoundResource<Items, List<MovieItem>>(appExecutors){
+            override fun loadFromDB(): LiveData<Items> {
+                return localDataSource.getMovie(Id)
+            }
+
+            override fun shouldFetch(data: Items?): Boolean {
+                return data == null
+            }
+
+            override fun createCall(): LiveData<ApiResponse<List<MovieItem>>> {
+                return remoteDataSource.getMovies()
+            }
+            override fun saveCallResult(data: List<MovieItem>) {
+                lateinit var movieItem: Items
+                for (movie in data){
+                    if (Id == movie.id){
+                        movieItem = Items(
+                            movie.id,
+                            movie.title,
+                            movie.releaseDate,
+                            movie.originalTitle,
+                            movie.popularity,
+                            movie.voteAverage,
+                            movie.overview,
+                            movie.posterPath,
+                            isFavorite = false,
+                            isTvShow = false
+                        )
+                    }
+                }
+                localDataSource.updateItems(movieItem)
+            }
+        }.asLiveData()
+    }
+
+    override fun getAllTvShows(sort : String): LiveData<Resource<PagedList<Items>>> {
+        return object :
+            NetworkBoundResource<PagedList<Items>, List<TvShowItems>>(appExecutors){
+            override fun loadFromDB(): LiveData<PagedList<Items>> {
+                val config = PagedList.Config.Builder()
+                    .setEnablePlaceholders(false)
+                    .setInitialLoadSizeHint(4)
+                    .setPageSize(4)
+                    .build()
+                return LivePagedListBuilder(localDataSource.getAllTvShows(sort), config).build()
+            }
+
+            override fun shouldFetch(data: PagedList<Items>?): Boolean {
+                return data == null || data.isEmpty()
+            }
+
+            override fun createCall(): LiveData<ApiResponse<List<TvShowItems>>> {
+                return remoteDataSource.getTvShow()
+            }
+
+            override fun saveCallResult(data: List<TvShowItems>) {
+                val tvList = ArrayList<Items>()
+                for(response in data){
+                    val tv = Items(
+                        response.id,
+                        response.name,
+                        response.firstAirDate,
+                        response.originalTitle,
+                        response.popularity,
+                        response.voteAverage,
+                        response.overview,
+                        response.posterPath,
+                        isFavorite = false,
+                        isTvShow = true
+                    )
+                    tvList.add(tv)
+                }
+                localDataSource.insertItems(tvList)
+            }
+        }.asLiveData()
+    }
+
+    override fun getTvShowsById(Id: Int): LiveData<Resource<Items>> {
+        return object :
+            NetworkBoundResource<Items, List<TvShowItems>>(appExecutors){
+            override fun loadFromDB(): LiveData<Items> {
+                return localDataSource.getTvShow(Id)
+            }
+
+            override fun shouldFetch(data: Items?): Boolean {
+                return data ==null
+            }
+
+            override fun createCall(): LiveData<ApiResponse<List<TvShowItems>>> {
+                return remoteDataSource.getTvShow()
+            }
+
+            override fun saveCallResult(data: List<TvShowItems>) {
+                lateinit var item : Items
+                for(tv in data){
+                    if(Id == tv.id){
+                        item = Items(
+                            tv.id,
+                            tv.name,
+                            tv.firstAirDate,
+                            tv.originalTitle,
+                            tv.popularity,
+                            tv.voteAverage,
+                            tv.overview,
+                            tv.posterPath,
+                            isFavorite = false,
+                            isTvShow = true
+                        )
+                    }
+                }
+                localDataSource.updateItems(item)
+            }
+        }.asLiveData()
+    }
+
+    override fun getFavoriteMovies(): LiveData<PagedList<Items>> {
+        val config = PagedList.Config.Builder()
+            .setEnablePlaceholders(false)
+            .setInitialLoadSizeHint(4)
+            .setPageSize(4)
+            .build()
+        return LivePagedListBuilder(localDataSource.getAllfavoriteMovies(), config).build()
+    }
+
+    override fun getFavoriteTvShow(): LiveData<PagedList<Items>> {
+        val config = PagedList.Config.Builder()
+            .setEnablePlaceholders(false)
+            .setInitialLoadSizeHint(4)
+            .setPageSize(4)
+            .build()
+        return LivePagedListBuilder(localDataSource.getAllFavoriteTvShow(), config).build()
+    }
+
+    override fun setToFavorite(item: Items, state: Boolean) {
+        return appExecutors.diskIO().execute{
+            localDataSource.setFavorite(item, state)
+        }
+    }
+
 }
--- Submission3BAJPDicoding/app/src/test/java/com/example/submission3bajpdicoding/data/source/ItemRepositoryTest.kt
+++ Submission3BAJPDicoding/app/src/test/java/com/example/submission3bajpdicoding/data/source/ItemRepositoryTest.kt
@@ -1,4 +1,140 @@
 package com.example.submission3bajpdicoding.data.source
 
-class ItemRepository {
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.MutableLiveData
+import com.example.submission3bajpdicoding.data.source.local.LocalDataSource
+import com.example.submission3bajpdicoding.data.source.remote.RemoteDataSource
+import com.example.submission3bajpdicoding.utilities.AppExecutors
+import com.example.submission3bajpdicoding.utilities.DataDummy
+import com.example.submission3bajpdicoding.utilities.SortUtils
+import org.junit.Rule
+import androidx.paging.DataSource
+import com.example.submission3bajpdicoding.data.source.local.entity.Items
+import com.example.submission3bajpdicoding.util.LiveDataTestUtils
+import com.example.submission3bajpdicoding.util.PagedLIstUtils
+import com.example.submission3bajpdicoding.vo.Resource
+import junit.framework.Assert.assertEquals
+import junit.framework.Assert.assertNotNull
+import org.junit.Test
+import org.mockito.Mockito.*
+
+class ItemRepositoryTest {
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
+
+    private val sort = SortUtils.NEWEST
+
+    private val remote = mock(RemoteDataSource::class.java)
+    private val local = mock(LocalDataSource::class.java)
+    private val appExecutors = mock(AppExecutors::class.java)
+
+    private val itemRepository = FakeItemRepository(remote, local, appExecutors)
+
+    private val movieResponse = DataDummy.generateDummyMovies()
+    private val movieId = movieResponse[0].id
+
+    private val tvResponse = DataDummy.generateDummyTvShow()
+    val tvId = tvResponse[0].id
+
+    @Test
+    fun getAllMovies(){
+        val dummyMovies = mock(DataSource.Factory::class.java) as DataSource.Factory<Int, Items>
+        `when`(local.getAllMovies(sort)).thenReturn(dummyMovies)
+        itemRepository.getAllMovies(sort)
+
+        val movieItem = Resource.success(PagedLIstUtils.mockPagedList(DataDummy.generateDummyMovies()))
+        verify(local).getAllMovies(sort)
+        assertNotNull(movieItem.data)
+        assertEquals(movieResponse.size.toLong(), movieItem.data?.size?.toLong())
+    }
+
+    @Test
+    fun getMovieById(){
+        val dummyMovie = MutableLiveData<Items>()
+        dummyMovie.value = DataDummy.generateDummyMovies()[0]
+        `when`(local.getMovie(movieId!!)).thenReturn(dummyMovie)
+
+        val movieItem = LiveDataTestUtils.getValue(itemRepository.getMovieById(movieId)).data
+        verify(local).getMovie(movieId)
+        val movieResponse = movieResponse[0]
+
+        assertNotNull(movieItem)
+        if (movieItem != null){
+            assertEquals(movieResponse.id, movieItem.id)
+            assertEquals(movieResponse.title, movieItem.title)
+            assertEquals(movieResponse.OriginalTitle, movieItem.OriginalTitle)
+            assertEquals(movieResponse.ReleaseDate, movieItem.ReleaseDate)
+            assertEquals(movieResponse.popularity, movieItem.popularity)
+            assertEquals(movieResponse.score, movieItem.score)
+            assertEquals(movieResponse.poster, movieItem.poster)
+            assertEquals(movieResponse.synopsis, movieItem.synopsis)
+            assertEquals(movieResponse.isFavorite, movieItem.isFavorite)
+            assertEquals(movieResponse.isTvShow, movieItem.isTvShow)
+        }
+    }
+
+    @Test
+    fun getAllTvShow(){
+        val dummyTv = mock(DataSource.Factory::class.java) as DataSource.Factory<Int, Items>
+        `when`(local.getAllTvShows(sort)).thenReturn(dummyTv)
+        itemRepository.getAllTvShows(sort)
+
+        val tvItems = Resource.success(PagedLIstUtils.mockPagedList((DataDummy.generateDummyTvShow())))
+        verify(local).getAllTvShows(sort)
+        assertNotNull(tvItems.data)
+        assertEquals(tvResponse.size.toLong(), tvItems.data?.size?.toLong())
+    }
+
+    @Test
+    fun getTvShowId(){
+        val dummyTv = MutableLiveData<Items>()
+        dummyTv.value = DataDummy.generateDummyTvShow()[0]
+        `when`(local.getTvShow(tvId!!)).thenReturn(dummyTv)
+
+        val tvItem = LiveDataTestUtils.getValue(itemRepository.getTvShowsById(tvId)).data
+        verify(local).getTvShow(tvId)
+        val tvResponse = tvResponse[0]
+
+        assertNotNull(tvItem)
+        if (tvItem != null){
+            assertEquals(tvResponse.id, tvItem.id)
+            assertEquals(tvResponse.title, tvItem.title)
+            assertEquals(tvResponse.OriginalTitle, tvItem.OriginalTitle)
+            assertEquals(tvResponse.ReleaseDate, tvItem.ReleaseDate)
+            assertEquals(tvResponse.popularity, tvItem.popularity)
+            assertEquals(tvResponse.score, tvItem.score)
+            assertEquals(tvResponse.poster, tvItem.poster)
+            assertEquals(tvResponse.synopsis, tvItem.synopsis)
+            assertEquals(tvResponse.isFavorite, tvItem.isFavorite)
+            assertEquals(tvResponse.isTvShow, tvItem.isTvShow)
+        }
+    }
+
+    @Test
+    fun getFavoriteMovies(){
+        val dataFavoriteMovies = mock(DataSource.Factory::class.java) as DataSource.Factory<Int, Items>
+        `when` (local.getAllfavoriteMovies()).thenReturn(dataFavoriteMovies)
+        itemRepository.getFavoriteMovies()
+
+        val favoriteItem = Resource.success(PagedLIstUtils.mockPagedList(DataDummy.generateDummyMovies()))
+        verify(local).getAllfavoriteMovies()
+        assertNotNull(favoriteItem.data)
+        assertEquals(movieResponse.size.toLong(), favoriteItem.data?.size?.toLong())
+    }
+
+    @Test
+    fun getFavoriteTvShow(){
+        val dataFavoriteTv = mock(DataSource.Factory::class.java) as DataSource.Factory<Int, Items>
+        `when`(local.getAllFavoriteTvShow()).thenReturn(dataFavoriteTv)
+        itemRepository.getFavoriteTvShow()
+
+        val favoriteTvItems = Resource.success(PagedLIstUtils.mockPagedList(DataDummy.generateDummyTvShow()))
+        verify(local).getAllFavoriteTvShow()
+        assertNotNull(favoriteTvItems.data)
+        assertEquals(tvResponse.size.toLong(), favoriteTvItems.data?.size?.toLong())
+    }
 }
+
+
+
+
